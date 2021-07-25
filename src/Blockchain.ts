fe4a7d8e511c1df8bf9a06f5a60c759a37a732b5
Marlom Girardi <marlom.girardi@gmail.com>
2021-07-25 22:08:30 +0100

    add proof of work method

--- src/Blockchain.ts
+++ src/Blockchain.ts
@@ -13,6 +13,7 @@ class Blockchain {
     this.createBlock(100, "0", "0");
   }
 
+  // TODO: Extract block to its own class
   createBlock(nonce: Nonce, previousBlockHash: Hash, hash: Hash): Block {
     const newBlock: Block = {
       index: this.chain.length + 1,
@@ -49,6 +50,21 @@ class Blockchain {
     const Sha256 = SHA("sha256");
     const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
     return Sha256.update(dataAsString).digest("hex");
+  }
+
+  /**
+   * @external https://en.bitcoin.it/wiki/Proof_of_work
+   */
+  proofOfWork(previousBlockHash: Hash, currentBlockData: Block["transactions"]) {
+    let nonce = -1;
+    let hash;
+
+    do {
+      nonce += 1;
+      hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    } while (!hash.startsWith("0000"));
+
+    return nonce;
   }
 }
 
